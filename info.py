7fefd9a682bfdcf2abe1b6d634f3ef7d2fc795f8
Creazybanda <workwithsaikat@gmail.com>
2024-08-03 18:07:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -71,7 +71,7 @@ CLONE_DATABASE_URI = environ.get('CLONE_DATABASE_URI', "") # Necessary If clone 
 PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Username Without @ or without https://t.me/ and Bot Is Admin With Full Right.
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Creazy_Discussion_Group')
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/creazymoviesurchgroup')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Creazy_World_Hub')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/How_to_open_file_to_link')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Creazy_Discussion_Group') # Support Chat Link Without https:// or @
