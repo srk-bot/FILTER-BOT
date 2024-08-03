15c712c283d53b2e15733099a3b9119d120fa7aa
Creazybanda <workwithsaikat@gmail.com>
2024-08-03 15:52:57 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "condemned-candra/vj-filter-bot")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
