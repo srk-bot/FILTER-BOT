a5890a8498f61b109dc28f719d1fafc25c189747
siriuscris <126855662+siriuscris@users.noreply.github.com>
2025-03-20 16:44:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -90,7 +90,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+bF8Lbvbhk8BhMDFl')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+sU254JcBd3llOTE1')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+AgnxABzePZMyMDg9) # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 't.me/+AgnxABzePZMyMDg9) # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Christy1998')
 
 # True Or False
